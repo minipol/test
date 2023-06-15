7c40b7d98e10b93864398bc73573ea08d2448cae
minipol <ninevoso@gmail.com>
2023-06-15 10:12:39 +0300

    1

--- internet.py
+++ internet.py
@@ -1,7 +1,7 @@
 # импорт библиотек
 import webview
 # создание окна
-# даём сылку на видео и т.д
+# даём cсылку на видео и т.д
 webview.create_window('интернет','https://www.youtube.com/watch?v=xm3YgoEiEDc',fullscreen=True)
 webview.start()
 # работает
