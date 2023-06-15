8d4f5eb36e31f1d093ae5914edc42a81848f26a9
minipol <ninevoso@gmail.com>
2023-06-15 09:50:01 +0300

    это видео

---
+++ internet.py
@@ -0,0 +1,7 @@
+# импорт библиотек
+import webview
+# создание окна
+# даём сылку на видео и т.д
+webview.create_window('интернет','https://www.youtube.com/watch?v=xm3YgoEiEDc',fullscreen=True)
+webview.start()
+# работает
